e3aa3504075696c08b634d16df1cda4398a858d0
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-02-23 17:14:31 -0700

    Increase menu label width to prevent text collision

--- internal/tui/hub.go
+++ internal/tui/hub.go
@@ -100,7 +100,7 @@ func renderMenuItem(w io.Writer, m list.Model, index int, item list.Item) {
 	label := menuItem.Label
 	desc := StyleHelp.Render(menuItem.Description)
 
-	display := fmt.Sprintf("%-20s   %s", label, desc)
+	display := fmt.Sprintf("%-25s   %s", label, desc)
 
 	if isSelected {
 		_, _ = fmt.Fprint(w, StyleHighlight.Render("› "+display))
